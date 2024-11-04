8fb74b79bfc0808639be60e967abffb978a26cc3
andreasAuffray <andreasauffray@outlook.fr>
2024-11-04 08:54:14 +0100

    test

--- public/login.php
+++ public/login.php
@@ -11,14 +11,17 @@
     <div class="card p-4 shadow-sm" style="width: 300px;">
       <h3 class="text-center mb-4">Connexion</h3>
       <form action="blog.html" method="GET">
-        <div class="mb-3">
+        
+      <div class="mb-3">
           <label for="username" class="form-label">Nom d'utilisateur</label>
           <input type="text" class="form-control" id="username" name="username">
         </div>
+
         <div class="mb-4">
           <label for="password" class="form-label">Mot de passe</label>
           <input type="password" class="form-control" id="password" name="password">
         </div>
+        
         <button type="submit" class="btn btn-primary w-100">Se connecter</button>
       </form>
     </div>
